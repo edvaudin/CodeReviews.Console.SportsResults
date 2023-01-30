4f1b5ae90cfcbbe24b2e800007045ddbefdcf940
Edward Vaudin <edward@vaudin.net>
2023-01-30 23:16:32 +0000

    Created GameFactory

--- Console.SportsResults.edvaudin/Models/Game.cs
+++ Console.SportsResults.edvaudin/Models/Game.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SportsResults;
 
-namespace SportsResults
+public class Game
 {
-    internal class Game
-    {
-        public string WinningTeam { get; set; }
-        public string LosingTeam { get; set; }
-        public int WinningScore { get; set; }
-        public int LosingScore { get; set; }
+    public string WinningTeam { get; set; }
+    public string LosingTeam { get; set; }
+    public int WinningScore { get; set; }
+    public int LosingScore { get; set; }
 
-        public string HighestPointScorer { get; set; }
-        public int HighestPlayerPoints { get; set; }
+    public string HighestPointScorer { get; set; }
+    public int HighestPlayerPoints { get; set; }
 
-        public string MostTotalRebounder { get; set; }
-        public int MostPlayerTotalRebounds { get; set; }
-        
-    }
+    public string MostTotalRebounder { get; set; }
+    public int MostPlayerTotalRebounds { get; set; }
+
 }
--- Console.SportsResults.edvaudin/Program.cs
+++ Console.SportsResults.edvaudin/Program.cs
@@ -1,35 +1,17 @@
 ﻿using HtmlAgilityPack;
 
-namespace SportsResults
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            var url = "https://www.basketball-reference.com/boxscores/";
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
-            var gameTables = doc.DocumentNode.SelectNodes("//div[@class='game_summary expanded nohover']");
-            List<Game> games = new List<Game>();
-            foreach ( var gameTable in gameTables )
-            {
-                var game = new Game()
-                {
-                    WinningTeam = gameTable.SelectSingleNode(".//tr[@class='winner']").SelectSingleNode(".//a").InnerText,
-                    WinningScore = int.Parse(gameTable.SelectSingleNode(".//tr[@class='winner']").SelectSingleNode(".//td[@class='right']").InnerText),
-                    LosingTeam = gameTable.SelectSingleNode(".//tr[@class='loser']").SelectSingleNode(".//a").InnerText,
-                    LosingScore = int.Parse(gameTable.SelectSingleNode(".//tr[@class='loser']").SelectSingleNode(".//td[@class='right']").InnerText),
-                    HighestPointScorer = gameTable.SelectNodes(".//table[@class='stats']/tbody/tr")[0].SelectSingleNode(".//td[contains(a, *)]").InnerText,
-                    HighestPlayerPoints = int.Parse(gameTable.SelectNodes(".//table[@class='stats']/tbody/tr")[0].SelectSingleNode(".//td[@class='right']").InnerText),
-                    MostTotalRebounder = gameTable.SelectNodes(".//table[@class='stats']/tbody/tr")[1].SelectSingleNode(".//td[contains(a, *)]").InnerText,
-                    MostPlayerTotalRebounds = int.Parse(gameTable.SelectNodes(".//table[@class='stats']/tbody/tr")[1].SelectSingleNode(".//td[@class='right']").InnerText),
+namespace SportsResults;
 
-                };
-                games.Add(game);
-                Console.WriteLine($"Game {gameTables.IndexOf(gameTable)}: {game.WinningTeam} [{game.WinningScore}] - {game.LosingTeam} [{game.LosingScore}]\n" +
-                    $"Highest point scorer: {game.HighestPointScorer} [{game.HighestPlayerPoints}]; Most Total Rebounds: {game.MostTotalRebounder} [{game.MostPlayerTotalRebounds}]");
-            }
-        }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var url = "https://www.basketball-reference.com/boxscores/";
+        var web = new HtmlWeb();
+        var doc = web.Load(url);
+        var gameTables = doc.DocumentNode.SelectNodes("//div[@class='game_summary expanded nohover']");
+
+        List<Game> games = GameFactory.CreateGamesFromHtmlNodeCollection(gameTables);
     }
 }
